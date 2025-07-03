7d10dca5775d69521ca13baceb59acd0b5668762
dan <danielkarongoi@gmail.com>
2025-07-03 17:14:36 +0300

    addec changes

--- server/app.py
+++ server/app.py
@@ -1,5 +1,19 @@
-#!/usr/bin/env python3
-
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1>Welcome to my page!</h1>'
+
+@app.route('/<string:username>')
+def user(username):
+    return f'<h1>Profile for {username}</h1>'
+
+@app.route('/search')
+def search():
+    term = request.args.get('term', 'nothing')
+    return f'<h1>You searched for: {term}</h1>'
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
